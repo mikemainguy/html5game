4439f159e6071a0c0e40e4edbe7639ad388fcae2
Mike Mainguy <mmainguy@redpointtech.com>
2012-01-14 10:56:23 -0600

    Fixed animation technique to speed things up.

--- html5game/base.js
+++ html5game/base.js
@@ -4,13 +4,14 @@ var game = function(id) {
     var me = {};
     me.startDate = new Date();
     me.count = 0;
-    me.clock = 1;
+    me.clock = 10;
     me.canvas = document.getElementById(id);
 
     me.canvas.setAttribute('width', me.canvas.clientWidth);
     me.canvas.setAttribute('height', me.canvas.clientHeight);
 
     me.degreeFactor = Math.PI / 180;
+    me.tweens = [];
 
     that.context = me.canvas.getContext('2d');
     that.entities = [];
@@ -31,39 +32,55 @@ var game = function(id) {
 
 
     that.entity = function(data) {
-        if (!data.destination) {
-            data.destination = {};
-        }
+
         if (!data.drawType) {
             data.drawType = 'rect';
         }
-        if (data.path){
+        if (data.path) {
             var min_x = 0;
             var min_y = 0;
             var max_x = 0;
             var max_y = 0;
             for (point in data.path) {
-                if (min_x > point.x) {min_x = point.x}
-                if (max_x < point.x) {max_x = point.x}
-                if (min_y > point.y) {min_y = point.y}
-                if (max_y < point.y) {max_y = point.y}
+                if (min_x > point.x) {
+                    min_x = point.x
+                }
+                if (max_x < point.x) {
+                    max_x = point.x
+                }
+                if (min_y > point.y) {
+                    min_y = point.y
+                }
+                if (max_y < point.y) {
+                    max_y = point.y
+                }
             }
             data.w = max_x - min_x;
             data.h = max_y - min_y;
             data.x = min_x;
             data.y = min_y;
         }
+        data.destination = {};
+        data.destination.unfinished = function() {
+            var dest = data.destination;
+            return (dest.x && !dest.x.done) ||
+                    (dest.y && !dest.y.done) ||
+                    (dest.rotation && !dest.rotation.done)
+        }
         data.move = function(x, y) {
             data.direction = {x:x,y:y};
+            me.tweens.push(data);
         }
 
         data.move_to = function(x, y, speed) {
             data.destination.x = me.interpolator(data.x, x, speed);
             data.destination.y = me.interpolator(data.y, y, speed);
+            me.tweens.push(data);
         }
 
-        data.rotate = function(new_angle, speed) {
+        data.rotate_to = function(new_angle, speed) {
             data.destination.rotation = me.interpolator(data.rotation, new_angle, speed);
+            me.tweens.push(data);
         }
 
         that.entities.push(data);
@@ -71,28 +88,35 @@ var game = function(id) {
     }
 
 
-    me.interpolator = function(start, finish, speed) {
-        var data = new Array();
+    me.interpolator = function(start, finish, duration) {
         var inter = {};
-        var count = ((speed * 1000) / me.clock);
-        var factor = (finish - start) / count;
-        for (i = count; i > 0; i--) {
-            data.push(start + (i * factor))
+        inter.done = false;
+        var currentValue = start;
+        var endValue = finish;
+        var totalTicks = ((duration * 1000) / me.clock);
+        var increment = (finish - start) / totalTicks;
+        inter.length = function() {
+            return Math.abs(endValue - currentValue);
         }
+        var currentTick = 0;
         inter.pop = function() {
-            if (data.length > 0) {
-                return data.pop();
+            currentTick++;
+            if (currentTick < totalTicks) {
+                currentValue += increment;
             } else {
-                return finish;
+                inter.done = true;
+
             }
+
+            return currentValue;
         }
         return inter;
     }
 
     that.draw = function() {
         that.clr();
-        for (i in that.entities) {
-            e = that.entities[i];
+        for (var i in that.entities) {
+            var e = that.entities[i];
 
             that.context.fillStyle = e.fillStyle || '#000000';
             that.context.save();
@@ -102,7 +126,7 @@ var game = function(id) {
             if (e.path) {
                 that.context.beginPath();
                 that.context.moveTo(e.path[0].x, e.path[0].y);
-                for (p = 1; p < e.path.length; p++) {
+                for (var p = 1; p < e.path.length; p++) {
                     that.context.lineTo(e.path[p].x, e.path[p].y);
                 }
                 that.context.stroke();
@@ -119,15 +143,20 @@ var game = function(id) {
     }
 
     that.animate = function() {
-        for (i in that.entities) {
-            e = that.entities[i];
+        var newTweens = [];
+        for (var i in me.tweens) {
+            var e = me.tweens[i];
             if (e.direction) {
                 e.x = me.wrap(e.x + e.direction.x || 0, me.canvas.width);
                 e.y = me.wrap(e.y + e.direction.y || 0, me.canvas.height);
             }
+
             if (e.destination) {
-                if (e.destination.x) {
+                if (!e.destination.x.done) {
                     e.x = me.wrap(e.destination.x.pop());
+
+                }
+                if (!e.destination.y.done) {
                     e.y = me.wrap(e.destination.y.pop());
                 }
                 if (e.destination.rotation) {
@@ -135,8 +164,13 @@ var game = function(id) {
                 }
 
             }
+            if (e.direction || e.destination.unfinished()) {
+                newTweens.push(e);
+            }
+
 
         }
+        me.tweens = newTweens;
         that.draw();
     }
 
@@ -153,9 +187,21 @@ var game = function(id) {
     me.debug = function() {
         var time = new Date();
         that.context.fillStyle = "#000000";
+        var line = 20;
+        that.context.fillText(1000 / ((time - me.startDate) / me.count) + ' fps', 20, line);
+        that.context.fillText(me.count + ' frames ', 20, line + 15);
+        that.context.fillText(that.entities.length + ' entities ', 20, line + 30);
 
-        that.context.fillText(1000 / ((time - me.startDate) / me.count) + ' fps', 20, 20);
-        that.context.fillText(me.count + ' frames ', 20, 40);
+        that.context.fillText(me.tweens.length + ' tweens ', 20, line + 45);
+        var total = 0.0;
+        for (var i in me.tweens) {
+            total += me.tweens[i].destination.x.length();
+            total += me.tweens[i].destination.y.length();
+            total += me.tweens[i].destination.rotation.length();
+
+        }
+        that.context.fillText(total + ' tween details ', 20, line + 60);
+
 
     }
 
